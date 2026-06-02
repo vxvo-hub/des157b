6dd3e2bc80c5b50466d0a898d2d0617a35f16e9d
Victoria Vo <victoriavo@Victorias-MacBook-Pro.local>
2026-06-02 10:17:29 -0700

    v2 and portal page cleanup

--- version2/script.js
+++ version2/script.js
@@ -2,25 +2,37 @@
     "use strict";
     console.log("reading js");
 
+
     const startBtn = document.getElementById("startBtn");
+
+    // screens
     const startscreen = document.getElementById("startscreen");
     const idealScreen = document.getElementById("idealScreen");
     const checkScreen = document.getElementById("checkScreen");
 
+    // buttons
     const idealNextBtn = document.getElementById("ideal-next-btn");
     const stBtn = document.getElementById("st-btn");
 
+    // input
     const screenTimeInput = document.getElementById("screentime-input");
 
     const screentimeDisplay = document.getElementById("screentime-display");
     let userScreenTime = 0;
+    
+    // draggable
+    const containers = document.querySelectorAll('.palette-grid, .timeline');
+
+    const screentimeStatCell = document.getElementById("stat-ideal-screen");
 
     startBtn.addEventListener("click", function() {
+        screentimeStatCell.classList.add("faded");
         startscreen.classList.add("offscreen");
         idealScreen.classList.remove("offscreen");
     });
 
     idealNextBtn.addEventListener("click", function() {
+        
         idealScreen.classList.add("offscreen");
         checkScreen.classList.remove("offscreen");
     });
@@ -34,8 +46,20 @@
         screentimeDisplay.textContent = "and " + userScreenTime + " hours of screen time";
         screentimeDisplay.classList.remove("offscreen");
     });
-    
 
+    if (containers.length > 0) {
+        const sortable = new Draggable.Sortable(containers, {
+            draggable: '.block-chip', 
+            mirror: {
+                constrainDimensions: true, 
+            },
+    });
+
+    // Optional: Log actions to see if it works!
+    sortable.on('sortable:start', () => console.log('Started dragging!'));
+    sortable.on('sortable:stop', () => console.log('Dropped the item!'));
+}
+    
     
 
 })();
